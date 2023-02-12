8827b42ea28480426f6f44d10cfc3f6793ec47ff
goldie <codegayle@gmail.com>
2023-02-11 20:30:34 -0500

    changed structure to confirm boxes and fixed some other issues

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length;
-var baseOptions = [];
-var lowerCase;
-var upperCase;
-var numeric;
-var specialCharacters;
+// var length
+//var baseOptions = [];
+//var lowerCase;
+//var upperCase;
+//var numeric;
+//var specialCharacters;
 var lowerPasswordCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperPasswordCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numericPasswordCharacters = ["1", "2","3", "4", "5", "6", "7", "8", "9"];
@@ -14,67 +14,59 @@ var specialPasswordCharacters = [",", "*"];
 
 // Write password to the #password input
 function writePassword() {
+  var baseOptions=[];
+  //parseInt
   var length = Math.round(prompt("How long would you like your password? (Choose a number between 8 and 128)"));
 
-  if (length < 8 || length >= 128) {
+  if (length < 8 || length > 128) {
     alert("Please choose a number between 8 and 128");
+    return  " ";
 
   } else {
     //Records whether user wants lowercase letters
-   lowerCase = prompt("Do you wish to include lowercase letters? (Yes or No)");
-    if (lowerCase !== "Yes" && lowerCase !== "No" && lowerCase !== "Y" && lowerCase !== "N" && lowerCase !== "n" && lowerCase !== "y" && lowerCase !=="yes" && lowerCase!== "no") {
-      alert("Please type Yes or No");
-       writePassword();
-    } else if (lowerCase === "Yes" || lowerCase === "Y" || lowerCase === "y" || lowerCase==="yes") {
-      lowerCase = true;
-    } else {
-      lowerCase = false;
+    var lowerCase = confirm("Do you wish to include lowercase letters? Press OK is so");
+    if (lowerCase === true) {
+      var choiceLc= baseOptions.concat(lowerPasswordCharacters);
+    } else{
+      choiceLc = baseOptions;
     }
-  }
-  
+   
+ 
 
   //Records whether user wants UpperCase letters
-  var upperCase = prompt("Do you wish to include uppercase letters? (Yes or No)");
-  if (upperCase !== "Yes" && upperCase !== "No" && upperCase !== "Y" && upperCase !== "N" && upperCase !== "n" && upperCase !== "y" && upperCase !== "yes" && upperCase !== "no") {
-    alert("Please type Yes or No");
-    writePassword();
-  } else if (upperCase === "Yes" || upperCase === "Y" || upperCase === "y" || upperCase ==="yes") {
-    upperCase = true;
-    
-  } else {
-    upperCase = false;
+  var upperCase = confirm("Do you wish to include uppercase letters? (Yes or No)");
+  if (upperCase === true){
+    var choiceUc = choiceLc.concat(upperPasswordCharacters);
+  } else{
+    choiceUc = choiceLc;
   }
 
-  console.log(typeof upperCase)
+ 
 
 //Records whether user wants numbers?
-  var numeric = prompt("Do you wish to include numbers? (Yes or No)");
-  if (numeric !== "Yes" && numeric !== "No" && numeric !== "Y" && numeric !== "N" && numeric !== "n" && numeric !== "y" && numeric!== "yes" && numeric !== "no") {
-    alert("Please type Yes or No");
-    writePassword();
-  } else if (numeric === "Yes" || numeric === "Y" || numeric === "y" || numeric === "yes") {
-    numeric = true;
-    
+  var numeric = confirm("Do you wish to include numbers? (Yes or No)");
+  if (numeric === true) {
+    var choiceN = choiceUc.concat(numericPasswordCharacters);
   } else {
-    numeric = false;
+    choiceN = choiceUc;
   }
   
-  console.log(typeof numeric)
 
   //Records whether user wants special characters?
-  var specialCharacters = prompt("Do you wish to include special characters (Yes or No)")
-  if (specialCharacters !== "Yes" && specialCharacters !== "No" && specialCharacters !== "Y" && specialCharacters !== "N" && specialCharacters !== "n" && specialCharacters !== "y" && specialCharacters !== "yes" && specialCharacters !== "no") {
-    alert("Please type Yes or No");
-    writePassword();
-  } else if (specialCharacters === "Yes" || specialCharacters === "Y" || specialCharacters === "y" || specialCharacters === "yes") {
-    specialCharacters = true;
-    
+  var specialCharacters = confirm("Do you wish to include special characters (Yes or No)")
+  if (specialCharacters === true) {
+    var choiceSc = choiceN.concat(specialPasswordCharacters)
   } else {
-    specialCharacters = false;
+    choiceSc = choiceN
+  }
+
+  if (lowerCase === false && upperCase === false && numeric === false && specialCharacters === false){
+    alert("must choose one type of character");
+    return "";
   }
 
   //Prepare array from options
-if (lowerCase === true) {
+/*if (lowerCase === true) {
   var oneChoice = baseOptions.concat(lowerPasswordCharacters);
 } else {
   var oneChoice = baseOptions
@@ -96,7 +88,8 @@ if (specialCharacters === true) {
  var lastChoice = thirdChoice.concat(specialPasswordCharacters)
 } else {
  var lastChoice = thirdChoice
-}
+} 
+*/
 
 
 
@@ -127,14 +120,14 @@ if (specialCharacters === true) {
 //Generates Password
 var passCode =[];
 
+//Generates Password based on User Choices
 function generatePassword (){
-  for (i=0; i< lastChoice.length; i++){
-    passCode.push(lastChoice[Math.floor(Math.random()*lastChoice.length)]);
-    passCode.splice(length)
-  }
+  for (i=0; i< length; i++){
+    passCode.push(choiceSc[Math.floor(Math.random()*choiceSc.length)]);
+  }   
+
 
 }
-
 generatePassword();
 var passwordText = document.querySelector("#password");
 console.log(passCode)
@@ -143,7 +136,7 @@ passwordText.value = passCode.join('');
 
 //passwordText.value = password;
 
-
+}
 
 
 //var password = generatePassword();
